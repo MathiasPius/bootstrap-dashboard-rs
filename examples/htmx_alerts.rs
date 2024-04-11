d5edc274e62b8494ac691609391ebb57047a4708
Mathias Pius <contact@pius.io>
2024-04-11 10:30:59 +0200

    Remove everything from alerts example which isn't critical to showcasing the htmx integration

--- examples/htmx_alerts.rs
+++ examples/htmx_alerts.rs
@@ -1,19 +1,14 @@
 use std::{sync::Arc, time::Duration};
 
 use axum::{
-    body::Body,
     extract::State,
-    http::{header, HeaderValue, Response, StatusCode},
     response::{Html, IntoResponse},
     routing::get,
     Router,
 };
 use bootstrap_dashboard::{
-    card::Card,
-    grid::{Breakpoint, Column, Row},
     htmx::{Dynamic, Hx, IntoDynamic, TriggerEvent},
-    icons, Alert, AlertList, Alerts, Dashboard, Group, IconLink, LinkAction, NavItem, Page,
-    PlainLink, Sidebar, SubGroup, UserInfo,
+    icons, Alert, AlertList, Alerts, Dashboard, Group, IconLink, LinkAction, Page, Sidebar,
 };
 use tokio::{net::TcpListener, sync::RwLock};
 
@@ -45,9 +40,7 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         .route("/", get(index))
-        .route("/configuration", get(configuration))
         .route("/alerts", get(alerts))
-        .route("/img/undraw_profile.svg", get(serve_profile_image))
         .merge(bootstrap_dashboard::files::serve_at(
             "/static-path/nested/*path",
         ))
@@ -62,61 +55,29 @@ async fn main() {
         .unwrap();
 }
 
-async fn serve_profile_image() -> impl IntoResponse {
-    Response::builder()
-        .status(StatusCode::OK)
-        .header(
-            header::CONTENT_TYPE,
-            HeaderValue::from_str("image/svg+xml").unwrap(),
-        )
-        .body(Body::from(
-            include_bytes!("img/undraw_profile.svg").as_slice(),
-        ))
-        .unwrap()
-}
-
 async fn index(state: State<Arc<RwLock<Vec<Alert>>>>) -> impl IntoResponse {
+    let sidebar = Sidebar::new("Dashboard", icons::fa::LAUGH_SQUINT).with_group(
+        Group::unlabeled().with_item(IconLink::new(
+            "Dashboard",
+            icons::fa::TACHOMETER_ALT,
+            LinkAction::to("/"),
+        )),
+    );
+
+    let alerts = Alerts {
+        alerts: alerts(state).await.into(),
+        show_all_url: Some("/notifications".into()),
+    };
+
+    let dashboard = Dashboard::new(sidebar)
+        .with_alerts(alerts)
+        .with_active_label("Dashboard")
+        .with_page_header("Dashboard")
+        .replace_content("This is the front page!");
+
     Html(
         Page::new("Dashboard", "/static-path/nested")
-            .with_content(
-                dashboard_builder(state)
-                    .await
-                    .with_active_label("Dashboard")
-                    .with_page_header("Dashboard")
-                    .replace_content("This is the front page!"),
-            )
-            .to_string(),
-    )
-}
-
-async fn configuration(state: State<Arc<RwLock<Vec<Alert>>>>) -> impl IntoResponse {
-    let row1 = Row::new()
-        .with_column(
-            Card::new("Hello world")
-                .with_header("First card")
-                .to_string(),
-        )
-        .with_column(
-            Column::new(Card::new("Hello world").to_string()).with_size(Breakpoint::ExtraLarge, 2),
-        )
-        .with_column(
-            Column::new(
-                Card::new("Hello world")
-                    .with_header("Large Card")
-                    .to_string(),
-            )
-            .with_size(Breakpoint::ExtraLarge, 7),
-        );
-
-    Html(
-        Page::new("My First Dashbaord", "/static-path/nested")
-            .with_content(
-                dashboard_builder(state)
-                    .await
-                    .with_active_label("Configuration")
-                    .with_page_header("Configuration")
-                    .replace_content(row1),
-            )
+            .with_content(dashboard)
             .to_string(),
     )
 }
@@ -127,71 +88,4 @@ async fn alerts(State(alerts): State<Arc<RwLock<Vec<Alert>>>>) -> Dynamic<AlertL
     AlertList(alerts).with_hx(
         Hx::get(format!("/alerts")).with_trigger(TriggerEvent::Every(Duration::from_secs(1))),
     )
-}
-
-async fn dashboard_builder(state: State<Arc<RwLock<Vec<Alert>>>>) -> Dashboard {
-    let sidebar = Sidebar::new("Dashboard", icons::fa::LAUGH_SQUINT)
-        .with_group(
-            Group::unlabeled()
-                .with_item(IconLink::new(
-                    "Dashboard",
-                    icons::fa::TACHOMETER_ALT,
-                    LinkAction::to("/"),
-                ))
-                .with_item(IconLink::new(
-                    "Configuration",
-                    icons::fa::COGS,
-                    LinkAction::to("/configuration"),
-                )),
-        )
-        .with_group(
-            Group::new("Another Group")
-                .with_item(NavItem::collapsible(
-                    "Collapsible",
-                    icons::fa::LIST,
-                    vec![
-                        SubGroup::unlabeled()
-                            .with_link(PlainLink::new("Placeholders!", LinkAction::to("/"))),
-                        SubGroup::new("First Subgroup")
-                            .with_link(PlainLink::new("Lorem", LinkAction::to("/")))
-                            .with_link(PlainLink::new("Ipsum", LinkAction::to("/"))),
-                        SubGroup::new("Second Subgroup")
-                            .with_link(PlainLink::new("Dolor", LinkAction::to("/")))
-                            .with_link(PlainLink::new("Sit Amet", LinkAction::to("/"))),
-                    ],
-                ))
-                .with_item(IconLink::new(
-                    "Plain Link",
-                    icons::fa::BELL,
-                    LinkAction::to("/"),
-                )),
-        );
-
-    let userinfo = UserInfo {
-        username: "John Smith".into(),
-        image: "/img/undraw_profile.svg".into(),
-        groups: vec![
-            vec![
-                IconLink::new("Profile", icons::fa::USER, LinkAction::to("/")).into(),
-                IconLink::new("Settings", icons::fa::COGS, LinkAction::to("/")).into(),
-                IconLink::new("Activity Log", icons::fa::LIST, LinkAction::to("/")).into(),
-            ],
-            vec![IconLink::new(
-                "Logout",
-                icons::fa::SIGN_OUT_ALT,
-                LinkAction::modal("logoutModal"),
-            )
-            .into()],
-        ],
-    };
-
-    let alerts = Alerts {
-        alerts: alerts(state).await.into(),
-        show_all_url: Some("/notifications".into()),
-    };
-
-    Dashboard::new(sidebar)
-        .with_copyright("Bootstrap Dashboard")
-        .with_alerts(alerts)
-        .with_userinfo(userinfo)
 }
