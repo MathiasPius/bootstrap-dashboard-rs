d6fe540bdc49f3457aea9577b7e948ed372e5060
Mathias Pius <contact@pius.io>
2024-04-26 09:39:46 +0200

    Add error constructor

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,17 @@
+use std::fmt::Display;
+
 #[derive(Template)]
 #[template(path = "error.html")]
 pub struct Error<T: Display> {
-    title: Cow<'static, str>,
-    message: T,
+    pub title: Cow<'static, str>,
+    pub message: T,
+}
+
+impl<T: Display> Error<T> {
+    pub fn new(title: &str, message: T) -> Self {
+        Self {
+            title: title.into(),
+            message,
+        }
+    }
 }
